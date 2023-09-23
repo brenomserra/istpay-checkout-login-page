f198ebc797191af30fadcd66c0f5c25144e92aa8
brenomserra <brenomserra@hotmail.com>
2023-09-22 21:28:55 -0300

    Refatoração estilo

---
+++ js/style.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                istpay: {
+                    laranja: '#FF651E',
+                    escuro: '#f9f9f9',
+                    branco: '#FFFFFF',
+                    hover: '#EA4A00'
+                },
+            },
+            fontFamily: {
+                inter: ['Inter', 'sans-serif']    
+            },
+        }
+    }
+}
